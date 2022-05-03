5017c28819ec64301827d4cbdf07286f3e49a654
Melissa Figallo <99562653+melissafigalloo@users.noreply.github.com>
2022-05-03 16:11:26 -0500

    javascript
    Add archivo.js en repositorio de GitHub

---
+++ miarchivo.js
@@ -0,0 +1,18 @@
+
+/* Ciclo FOR & WHILE */
+
+let pass = 'hola';
+while(pass != 'hola'){
+    pass = prompt('Introduzca su contrasena')}
+
+    console.log('Fin del bucle');
+
+    do { pass = prompt('Introduzca su contrasena')
+}
+while(pass != 'hola')
+
+
+
+
+
+
